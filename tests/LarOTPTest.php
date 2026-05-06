08b1c0bbf7f179720e9e45b77f0a29601d071298
Lambda1001 <kvinandk11@gmail.com>
2026-05-06 16:16:34 +0300

    Feat: Addition of Files for testing purposes

---
+++ tests/LarOTPTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace circlesandlambdas\larotp\Tests;
+
+use circlesandlambdas\larotp\LarOTP;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(LarOTP::class)]
+class LarOTPTest extends TestCase
+{
+    public function test_encoded_key_should_be_created()
+    {
+        $config = [
+            'key_length' => 32,
+        ];
+        $larOTP = new LarOTP($config);
+
+        $encoded_key = $larOTP->createSecretKey();
+
+        $this->assertNotEmpty($encoded_key);
+        $this->assertIsString($encoded_key);
+    }
+
+    public function test_encoded_key_should_be_base64()
+    {
+        $config = [
+            'key_length' => 32,
+        ];
+        $larOTP = new LarOTP($config);
+
+        $encoded_key = $larOTP->createSecretKey();
+
+        $decoded_key = base64_decode($encoded_key, true);
+
+        $this->assertIsString($decoded_key);
+    }
+}
